fbc72f9ed4ba8688320c28eb21ad5027ef012236
keiitikanemoto <lincolnkanemoto@zohomail.com>
2024-06-14 22:19:26 -0300

    Finalizar popup

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,49 @@
-console.log("Olá Mundo!!");
+const user = document.querySelector(".perfil_text");
+const aboutUser = document.querySelector(".perfil_subText");
+let nameInput = document.querySelector(".popup__name");
+let jobInput = document.querySelector(".popup__about");
+const popupDisplay = document.querySelector(".popup")
+const closeButton = document.querySelector(".popup__close_window-button");
+const editButton = document.querySelector(".perfil_button_edit");
+let formElement = document.querySelector(".popup__container_form");
+
+
+closeButton.addEventListener('click', event => {
+  console.log(event.target);
+})
+
+editButton.addEventListener('click', event => {
+  console.log(event.target);
+})
+
+function displayBlock(popupDisplay, popupOpenClass) {
+  popupDisplay.classList.add(popupOpenClass);
+  if (popupOpenClass === "popup__opened") {
+    nameInput.value = user.textContent;
+    jobInput.value = aboutUser.textContent;
+  }
+}
+
+function displayNone (popupDisplay, popupOpenClass) {
+  popupDisplay.classList.remove(popupOpenClass);
+}
+
+closeButton.addEventListener("click", () => displayNone(popupDisplay, "popup__opened"));
+
+editButton.addEventListener("click", () => displayBlock(popupDisplay, "popup__opened"))
+
+function handleProfileFormSubmit (evt) {
+  evt.preventDefault();
+
+ 
+
+  if (nameInput.value != "" && jobInput.value != "") {
+    user.textContent = nameInput.value;
+    aboutUser.textContent = jobInput.value;
+  } 
+
+}
+
+formElement.addEventListener('submit', handleProfileFormSubmit);
+
+
